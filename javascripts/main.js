65404ccd75c0df5222fef9fa2ae843211c1c3aa7
Ben Greaves <bsgreaves@gmail.com>
2017-05-14 02:18:21 -0500

    Done

---
+++ javascripts/main.js
@@ -0,0 +1,37 @@
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
+
+app.controller("mushroomCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+
+	$scope.mushrooms = [];
+	$scope.poisonFilter = undefined;
+
+	let getFBMushrooms = () => {
+		let mushrooms = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+			.then(fbMushrooms => {
+				fbMushrooms = fbMushrooms.data;
+				Object.keys(fbMushrooms).forEach((key) => {
+					fbMushrooms[key].id = key;
+					mushrooms.push(fbMushrooms[key]);
+				});
+				mushrooms.forEach(each => {
+					each.name = _.capitalize(each.name);
+				});
+				resolve(mushrooms);
+			})
+			.catch(error => {reject(error);});
+		});
+	};
+
+	getMushrooms = () => {
+		getFBMushrooms()
+		.then(fbMushrooms => {$scope.mushrooms = fbMushrooms;})
+		.catch(error => console.log("error in getMushrooms", error));
+	};
+
+	getMushrooms();
+
+});
